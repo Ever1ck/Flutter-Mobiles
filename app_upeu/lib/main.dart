bf98affd1d561b62faea715afd8fd922d5e55453
Ever1ck <64207368+Ever1ck@users.noreply.github.com>
2022-11-07 18:15:32 -0500

    Todo lo demas

---
+++ app_upeu/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:app_upeu/login/login_google.dart';
+import 'package:app_upeu/theme/AppTheme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "UpeU",
+      theme: AppTheme.themeData,
+      debugShowCheckedModeBanner: false,
+      home: MainLogin(),
+    );
+  }
+}
---
+++ app_upeu/lib/util/UrlApi.dart
@@ -0,0 +1,4 @@
+class UrlApi {
+  UrlApi._();
+  static const String urlApix = "http://172.23.11.62:8000";
+}
